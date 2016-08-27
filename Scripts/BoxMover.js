7618192b5c61c8f10858af5b3767c6706427e177
Danny Ritter <danny.ritter@icloud.com>
2016-08-26 20:54:47 -0500

    add basic script

---
+++ Scripts/BoxMover.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var speed = 0;
+var strafe_speed = 0;
+
+function Start () {
+	
+};
+
+function Update () {
+	Debug.Log("walk speed is " + speed);
+	Debug.Log("strafe speed is " + strafe_speed);
+};
