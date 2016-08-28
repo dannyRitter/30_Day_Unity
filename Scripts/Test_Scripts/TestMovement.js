fd534da161f380adb8dd2a0177dabb88a338e26a
Danny Ritter <danny.ritter@icloud.com>
2016-08-27 22:47:11 -0500

    add test movement script

---
+++ Scripts/Test_Scripts/TestMovement.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+
+function Start () {
+
+}
+
+function Update ()
+{
+    if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+}
